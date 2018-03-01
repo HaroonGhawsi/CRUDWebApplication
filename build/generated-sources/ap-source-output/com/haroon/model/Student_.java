51059c26d8253ac24b88d021ae5567e8268f7524
Haroon Ghawsi <haroon.ghawsi@frs.de>
2018-03-01 15:40:17 +0100

    Initial Commit

---
+++ build/generated-sources/ap-source-output/com/haroon/model/Student_.java
@@ -0,0 +1,16 @@
+package com.haroon.model;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-03-01T15:31:47")
+@StaticMetamodel(Student.class)
+public class Student_ { 
+
+    public static volatile SingularAttribute<Student, Integer> studentId;
+    public static volatile SingularAttribute<Student, String> firstname;
+    public static volatile SingularAttribute<Student, Integer> yearLevel;
+    public static volatile SingularAttribute<Student, String> lastname;
+
+}
